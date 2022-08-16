51e707f080c6ba2f84e8230532b2f1ab4a9964f4
JEYASHRI4 <jeyashri467@gmail.com>
2022-08-16 21:55:44 +0530

    Calculator

---
+++ main.js
@@ -0,0 +1,32 @@
+function add()
+{
+    var result=Number(document.getElementById("firstno").value)+
+    Number(document.getElementById("secondno").value);
+alert(result);
+}
+function sub()
+{
+    var result=Number(document.getElementById("firstno").value)-
+    Number(document.getElementById("secondno").value);
+alert(result);
+}
+function multi()
+{
+    var result=Number(document.getElementById("firstno").value)*
+    Number(document.getElementById("secondno").value);
+alert(result);
+}
+function div()
+{
+    var result=Number(document.getElementById("firstno").value)/
+    Number(document.getElementById("secondno").value);
+alert(result);
+}
+function square()
+{
+    var result=Number(document.getElementById("firstno").value)*Number(document.getElementById("firstno").value);
+    alert(result);
+    var result=Number(document.getElementById("secondno").value)*Number(document.getElementById("secondno").value);
+alert(result);
+}
+
